788679a2a1ec5e1d0d2ca38622c3e2528a182c9d
SamM <s.mcdonald@outlook.com.au>
2025-06-07 09:13:14 +1000

    feat: added Util and files tab for Commit window

--- ConventionalCommits/plugin.kts
+++ ConventionalCommits/plugin.kts
@@ -23,11 +23,33 @@ class ConventionalCommitsPanel(private val project: Project) : JPanel(BorderLayo
     }
 }
 
+class FilesPanel(private val project: Project) : JPanel(BorderLayout()) {
+    val buttonPanel = JPanel(GridLayout(5, 2, 10, 10))
+    init {
+        add(buttonPanel, BorderLayout.SOUTH)
+    }
+    private fun actionHandler() {
+        // action here
+    }
+}
+
+class UtilPanel(private val project: Project) : JPanel(BorderLayout()) {
+    val buttonPanel = JPanel(GridLayout(5, 2, 10, 10))
+    init {
+        add(buttonPanel, BorderLayout.SOUTH)
+    }
+    private fun actionHandler() {
+        // action here
+    }
+}
+
 project?.let { currentProject ->
     val disposable = Disposer.newDisposable("foxJCommitsPanel")
     Disposer.register(pluginDisposable, disposable)
 
     val conventionalCommitsPanel = ConventionalCommitsPanel(currentProject)
+    val utilPanel = UtilPanel(currentProject)
+    val changedFilesPanel = FilesPanel(currentProject)
 
     val placeholderPanel = JPanel()
     val toolWindow = currentProject.registerToolWindow(
@@ -41,9 +63,13 @@ project?.let { currentProject ->
     val contentFactory = ContentFactory.SERVICE.getInstance()
 
     val ccContent = contentFactory.createContent(conventionalCommitsPanel, "Commit", false)
+    val filesContent = contentFactory.createContent(changedFilesPanel, "Files", false)
+    val utilContent = contentFactory.createContent(utilPanel, "Utilities", false)
 
     contentManager.removeAllContents(true)
     contentManager.addContent(ccContent)
+    contentManager.addContent(filesContent)
+    contentManager.addContent(utilContent)
 
     Disposer.register(disposable, Disposable {
         println("🔻 Conventional Commits plugin stopped.")
